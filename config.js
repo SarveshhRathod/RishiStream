a3281b74a937678e0b0a6a95f17ae630db0574ad
Sarvesh Asatkar <sarveshh.asatkarr@gmail.com>
2026-08-05 18:49:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
+// ========================================================
+// STREAMGOD GLOBAL CONFIGURATION
+// ========================================================
 const CONFIG = {
-  // आपका JSON Endpoint
-  JSON_URL: "https://sonujson-devloper.vercel.app/Data/prime.json",
-  
-  // लोगो URLs
-  TITU_LOGO: "https://assetstitu.vercel.app/TituLogo.svg",
-  EV_LOGO: "https://raw.githubusercontent.com/saptarshiorg/saptarshiorg.github.io/refs/heads/main/magenta.png",
-  FANCODE_LOGO: "https://www.fancode.com/skillup-uploads/fc-web-logo/fc_logo_white_bg.svg",
-  FALLBACK_IMG: "https://www.fancode.com/skillup-uploads/cms-media/Cricket_Fallback_Old_match-card.jpg",
-  
-  // JW Player Key
-  JW_KEY: "XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo",
-  
-  // एन्क्रिप्शन टोकन सुरक्षा
-  SECRET_SALT: "TITU_EV_SECURE_2026"
+    // Live Stream JSON API URL
+    STREAM_JSON_URL: "https://sonujson-devloper.vercel.app/Data/prime.json",
+
+    // Telegram Channel Link
+    TELEGRAM_CHANNEL_URL: "https://t.me/StreamGodLive",
+
+    // Watch Ad to Unlock Direct Link URL
+    AD_DIRECT_LINK_URL: "https://www.effectivecpmnetwork.com/bsjuuf6hiv?key=ed4cad18c264ae7e112ec18db6753149"
 };
